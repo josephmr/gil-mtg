4455c3b4509dd3bb829b9f58b64e4243101be86c
Joseph Rollins <rollins.joseph@gmail.com>
2022-05-24 23:10:46 -0700

    Add Processor for managing websocket connection
    Beginning to handle some error edge cases -- many to go.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use serde::{Deserialize, Serialize};
 use serde_repr::*;
 use std::sync::{Arc, Mutex};
 use thiserror::Error;
-use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+use tokio::{
+    net::TcpStream,
+    sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+};
 use tokio_tungstenite::{
-    connect_async, tungstenite::client::IntoClientRequest, tungstenite::Message as WsMessage,
+    connect_async, tungstenite::client::IntoClientRequest, tungstenite::Error as TsError,
+    tungstenite::Message as TsMessage, MaybeTlsStream, WebSocketStream,
 };
 use tracing::{debug, error, warn};
 
@@ -64,9 +68,101 @@ enum Error {
     InvalidToken,
 }
 
+#[derive(Error, Debug)]
+enum ConnectingError {
+    #[error("invalid Guilded bearer token")]
+    InvalidToken,
+    #[error("error establishing websocket connection")]
+    Establishing(#[from] tokio_tungstenite::tungstenite::Error),
+}
+
 struct Client {
     sender: Mutex<Option<UnboundedSender<Event>>>,
     token: String,
+}
+
+enum ProcessorAction {}
+
+struct Processor {
+    events: UnboundedSender<Event>,
+    controls: UnboundedSender<ProcessorAction>,
+    ws_stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
+}
+
+impl Processor {
+    async fn new(
+        token: &str,
+        events: UnboundedSender<Event>,
+        controls: UnboundedSender<ProcessorAction>,
+    ) -> Result<Self, ConnectingError> {
+        let mut req = "wss://api.guilded.gg/v1/websocket"
+            .into_client_request()
+            .expect("failed to parse websocker uri");
+        req.headers_mut().append(
+            "Authorization",
+            format!("Bearer {}", token,)
+                .parse()
+                .map_err(|_| ConnectingError::InvalidToken)?,
+        );
+        debug!("processor attempting to connect to Guilded websocket");
+        let (ws_stream, _) = connect_async(req).await?;
+        debug!("processor websocket connected successfully");
+
+        Ok(Processor {
+            events,
+            controls,
+            ws_stream,
+        })
+    }
+
+    async fn run(&mut self) {
+        while let Some(message) = self.ws_stream.next().await {
+            match message {
+                Ok(message) => self.handle_message(message),
+                Err(_) => todo!(),
+            }
+        }
+
+        // Received a none, this should be interpreted as a force close of the
+        // socket and attempt a reconnect
+        error!("processor stream ended unexpectedly")
+    }
+
+    fn handle_event(&self, text: String) {
+        // First pass just decode the "op" code from the event. This determined
+        // if it will fully parse into a Message or if it is a Welcome message.
+        let op = serde_json::from_str::<Op>(&text);
+        match op {
+            Ok(Op { op: OpCode::Event }) => match serde_json::from_str::<Message>(&text) {
+                Ok(event) => self
+                    .events
+                    .send(event.d)
+                    .expect("processor failed to send event"),
+                Err(err) => warn!(?text, ?err, "failed to decode event"),
+            },
+            Ok(Op {
+                op: OpCode::Welcome,
+            }) => debug!(?text, "received welcome message"),
+            Ok(op) => warn!(?text, ?op, "failed to decode event, unrecognized op code"),
+            Err(err) => error!(?err, "failed to decode Op"),
+        }
+    }
+
+    fn handle_message(&self, message: TsMessage) {
+        match message {
+            TsMessage::Text(text) => self.handle_event(text),
+            // TODO: implement client side heartbeats + pong handling
+            TsMessage::Pong(_) => todo!(),
+            // TODO: handle receiving close frame from Guilded with reconnect
+            TsMessage::Close(_frame) => todo!(),
+            // Ignore Ping, tungstenite will automatically Pong
+            TsMessage::Ping(_) => (),
+            // Binary message should not be sent by Guilded
+            TsMessage::Binary(_) => warn!("received binary message, ignoring"),
+            // Frame should not be received per tungstenite docs
+            TsMessage::Frame(_) => error!("received raw Message::Frame, ignoring"),
+        }
+    }
 }
 
 type LibResult<T> = core::result::Result<T, Error>;
@@ -82,49 +178,22 @@ impl Client {
     }
 
     // TODO: Use type state to manage connection state (connected|connecting|not_connected)
-    async fn start(&mut self) -> LibResult<Events> {
-        let mut req = "wss://api.guilded.gg/v1/websocket"
-            .into_client_request()
-            .unwrap();
-        req.headers_mut().append(
-            "Authorization",
-            format!("Bearer {}", self.token,)
-                .parse()
-                .map_err(|_| Error::InvalidToken)?,
-        );
+    async fn start(&mut self) -> Result<Events, ConnectingError> {
         let (tx, rx) = unbounded_channel();
         self.sender = Mutex::new(Some(tx));
-        let sender = self.sender.lock().expect("sender poisoned").take().unwrap(); // TODO: handle already started -- take() will return None
+        let sender = self
+            .sender
+            .lock()
+            .expect("sender poisoned")
+            .take()
+            .expect("already started"); // TODO: handle already started -- take() will return None
 
-        let (mut ws_stream, _) = connect_async(req).await?;
+        let (ptx, _prx) = unbounded_channel();
+        let mut processor = Processor::new(&self.token, sender, ptx).await?;
         tokio::spawn(async move {
-            while let Some(message) = ws_stream.next().await {
-                match Client::parse(message.unwrap()) {
-                    Ok(Some(event)) => sender.send(event).unwrap(),
-                    Ok(None) => continue,
-                    Err(e) => warn!("Failed to handle an unknown websocket message: {:#?}", e),
-                }
-            }
+            processor.run().await;
         });
         Ok(rx)
-    }
-
-    // TODO: split handling of tungstenite::Message's from parsing into Guilded Events
-    fn parse(message: tokio_tungstenite::tungstenite::Message) -> LibResult<Option<Event>> {
-        match message {
-            WsMessage::Text(message) => {
-                if let Op { op: OpCode::Event } = serde_json::from_str(&message)? {
-                    Ok(Some(serde_json::from_str::<Message>(&message)?.d))
-                } else {
-                    Ok(None)
-                }
-            }
-            _ => {
-                // TODO: handle heartbeats, etc
-                debug!(ws_message = ?message, "Received websocket message other than Text");
-                Ok(None)
-            }
-        }
     }
 }
 
@@ -134,15 +203,16 @@ async fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt::init();
 
     // Set up Websocket Client for Guilded
-    let token = std::env::var("GUILDED_BEARER_TOKEN").unwrap();
+    let token = std::env::var("GUILDED_BEARER_TOKEN")
+        .expect("GUILDED_BEARER_TOKEN env var must be provided");
     let mut client = Client::new(&token).await?;
     let mut events = client.start().await?;
-    debug!("listening");
 
     // Set up Scryfall API HTTP client
     use mtg::http::Client as HttpClient;
     let http_client = HttpClient::new(&token);
-    let re = Arc::new(Regex::new(r"\[\[(?P<query>.*?)\]\]").unwrap());
+    let re =
+        Arc::new(Regex::new(r"\[\[(?P<query>.*?)\]\]").expect("failed to create message regex"));
 
     loop {
         if let Some(Event::ChatMessageCreated { message, .. }) = events.recv().await {
